3090170794e1edad5dc591465f12dd2c8d392779
VolhaRutkouskaya <141616785+VolhaRutkouskaya@users.noreply.github.com>
2023-08-27 15:08:36 +0300

    Update script.js
    * при написании текста поле очищается;
    * текст добавляется нажатием Enter;
    *  при нажатии левой кнопкой мыши добавить зачеркивание или снять

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
+const sendInput = document.querySelector('#input');
+const groceries = document.querySelector('.groceries');
+
+
+sendInput.addEventListener('keydown',function(event){
+    if (event.key == 'Enter') {
+    const messagesPurchase = sendInput.value;
+    
+    const firstPurchase = document.createElement('div');
+    
+    firstPurchase.classList.add('shopping');
+
+    firstPurchase.addEventListener('click', function(){
+    firstPurchase.classList.toggle('done');
+    })
+    
+    firstPurchase.textContent = messagesPurchase; 
+
+
+    if (messagesPurchase != '') {
+        groceries.append(firstPurchase);
+    }
+
+    sendInput.value = '';
+
+    }
+     
+})
+
+
+
+
+
+
+
+
+
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
